b06bace002d0e3ea4580238212e2082d5e681a6c
oleg.iakovenko <iakovenkoua@gmail.com>
2023-12-20 18:28:26 +0200

    Initial commit

---
+++ src/main/java/com/bobocode/ht19dbschemas/Ht19DbSchemasApplication.java
@@ -0,0 +1,13 @@
+package com.bobocode.ht19dbschemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ht19DbSchemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ht19DbSchemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bobocode/ht19dbschemas/Ht19DbSchemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobocode.ht19dbschemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ht19DbSchemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
